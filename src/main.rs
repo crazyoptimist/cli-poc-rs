514836409d66b3afdf3d9d516c28420ab585d9bc
crazyoptimist <hey@crazyoptimist.net>
2024-03-05 04:28:24 -0600

    nit: extract to run, rename to build from new

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -11,6 +11,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
@@ -23,7 +27,7 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
